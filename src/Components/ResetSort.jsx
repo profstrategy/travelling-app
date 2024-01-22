3704283d408c65c2aba82a77e9f31c3b64295751
strategy <ojobalaro2018@gmail.com>
2024-01-22 02:16:57 -0800

    updated travelling app

--- src/Components/ResetSort.jsx
+++ src/Components/ResetSort.jsx
@@ -8,18 +8,19 @@ const ResetSort = ({ onClearitems, sortItem, setSortItem, handleReceipt, numItem
         <>
             <div className='flex justify-between items-center gap-2 flex-wrap'>
 
-            {numItems ? (
-                        <Button
-                            onClick={handleReceipt}
-                            bgColor='gradient-to-r from-cyan-500 to-blue-500'
-                            px={'6'}
-                            py={'2'}
-                            shadowCol={'zinc-300'}
-                            z_index={'50'}
-                            saturation={'blend-saturation'}
+                {numItems ? (
+                    <Button
+                        onClick={handleReceipt}
+                        bgColor='gradient-to-r from-cyan-500 to-blue-500'
+                        px={'6'}
+                        py={'2'}
+                        shadowCol={'zinc-300'}
+                        z_index={'50'}
+                        saturation={'blend-saturation'}
 
-                        >Your Receipt</Button>
-                    ) : null}
+                    >Your Receipt</Button>
+                ) : null}
+
 
                 <div className='flex gap-2'>
                     <h4 className='text-white hidden md:block'>Sort by</h4>
@@ -31,29 +32,24 @@ const ResetSort = ({ onClearitems, sortItem, setSortItem, handleReceipt, numItem
                     </select>
                 </div>
 
-                    <button onClick={onClearitems} className="bg-blend-saturation bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-1.5 rounded text-white col-span-1  h-fit ">Clear All</button>
+
+                {isReceipt ? (
+                    <>
+
+                        <div className='bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-3 rounded md:w-1/6 fixed bottom-28 md:bottom-10 w-3/6 left-40 '>
+                            <ul className='text-white'>
+                                <li className='border-b-2'>Total-items: {numItems}</li>
+                                <li className='border-b-2'>Packed-items: {numPacked}</li>
+                                <li className={`border-b-2 ${percentage < 50 ? 'text-red-600' : 'text-green-900'}`}>Percentage: {percentage}%</li>
+                            </ul>
+                        </div>
+
+                    </>
+                ) : null}
 
 
-                    {isReceipt ? (
-                        <>
-
-                            <div className='bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-3 rounded md:w-1/6 fixed bottom-28 md:bottom-10 w-2/6 '>
-                                <ul className='text-white'>
-                                    <li className='border-b-2'>Total-items: {numItems}</li>
-                                    <li className='border-b-2'>Packed-items: {numPacked}</li>
-                                    <li className={`border-b-2 ${percentage < 50 ? 'text-red-600' : 'text-green-900'}`}>Percentage: {percentage}%</li>
-                                </ul>
-                            </div>
-
-                        </>
-                    ) : null}
-
-
-
-                   
-
-                
-                </div>
+                <button onClick={onClearitems} className="bg-blend-saturation bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-1.5 rounded text-white col-span-1  h-fit ">Clear All</button>
+            </div>
         </>
     )
 }
